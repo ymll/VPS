e5845cd8e24940f0ba7827d9fc2d7888f906c44a
Chun Kit Chan <tommychan19910123@gmail.com>
2014-06-02 18:15:10 +0800

    Auto pairing sensor with Hardcoded pin

--- src/hk/edu/cuhk/ymll/vps/RfidSensorActivity.java
+++ src/hk/edu/cuhk/ymll/vps/RfidSensorActivity.java
@@ -1,5 +1,7 @@
 package hk.edu.cuhk.ymll.vps;
 
+import java.lang.reflect.InvocationTargetException;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -16,6 +18,7 @@ public class RfidSensorActivity extends Activity implements IRfidSensor {
 	private SensorBroadcastReceiver sensorBroadcastReceiver;
 
 	private String sensorAddress;
+	private String sensor_pin;
 	private static final int REQUEST_BLUETOOTH_ENABLE = 43839;
 	
 	@Override
@@ -30,11 +33,13 @@ public class RfidSensorActivity extends Activity implements IRfidSensor {
 		}
 		
 		sensorAddress = this.getResources().getString(R.string.sensor_address);
+		sensor_pin = this.getResources().getString(R.string.sensor_pin);
 	    sensorBroadcastReceiver = new SensorBroadcastReceiver();
 	    
 	    IntentFilter intentFilter = new IntentFilter();
 	    intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
 	    intentFilter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
+	    intentFilter.addAction("android.bluetooth.device.action.PAIRING_REQUEST");
 	    intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
 	    intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
 	    intentFilter.addAction(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
@@ -124,19 +129,34 @@ public class RfidSensorActivity extends Activity implements IRfidSensor {
 
 	@Override
 	public void pairSensor(BluetoothDevice device) {
-		System.out.println("pairSensor");		
+		System.out.println("pairSensor");
+		
+		//System.out.println(Arrays.toString(device.getClass().getMethods()));
+		if(device.getBondState() == BluetoothDevice.BOND_NONE){
+			try {
+				device.getClass().getMethod("createBond", (Class[]) null).invoke(device, (Object[]) null);
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}else if(device.getBondState() == BluetoothDevice.BOND_BONDED){
+			onSensorPaired();
+		}
 	}
 
 	@Override
 	public void onSensorPaired() {
-		// TODO Auto-generated method stub
-		
+		connectSensor();
 	}
 
 	@Override
 	public void connectSensor() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("connectSensor");		
 	}
 
 	@Override
@@ -164,9 +184,6 @@ public class RfidSensorActivity extends Activity implements IRfidSensor {
 			String action = intent.getAction();
 			
 			System.out.printf("SensorBroadcastReceiver.onReceive.Action: %s\n", action);
-			if(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED.equals(action)){
-				System.out.println(intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, -4));
-			}
 			
 			if(BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)){
 				int btState = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);				
@@ -174,13 +191,43 @@ public class RfidSensorActivity extends Activity implements IRfidSensor {
 					onBluetoothEnabled(true);
 			}
 			
-			if(BluetoothDevice.ACTION_FOUND.equals(action)){
+			else if(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED.equals(action)){
+				System.out.println(intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, -4));
+			}
+			
+			else if(BluetoothDevice.ACTION_FOUND.equals(action)){
 				BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 				System.out.println(String.format("%s - %s - %s - %s", device.getName(), device.getAddress(), device.getBondState(), device.getBluetoothClass()));
 				System.out.println(String.format("%s <-> %s", sensorAddress, device.getAddress()));
 				if(sensorAddress.equalsIgnoreCase(device.getAddress())){
 					onSensorDiscovered(device);
 				}
+			}
+			
+			else if(BluetoothDevice.ACTION_BOND_STATE_CHANGED.equals(action)){
+				int bondState = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, BluetoothDevice.ERROR);
+				System.out.printf("Bound State: %d\n", bondState);
+				if(bondState == BluetoothDevice.BOND_BONDED){
+					onSensorPaired();
+				}
+			}
+			
+			else if("android.bluetooth.device.action.PAIRING_REQUEST".equals(action)){
+				BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+				try {
+					device.getClass().getMethod("setPin", byte[].class).invoke(device, sensor_pin.getBytes());
+					device.getClass().getMethod("setPairingConfirmation", boolean.class).invoke(device, true);
+					device.getClass().getMethod("cancelPairingUserInput").invoke(device);
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				} catch (NoSuchMethodException e) {
+					e.printStackTrace();
+				}
+
 			}
 		}
 	}
